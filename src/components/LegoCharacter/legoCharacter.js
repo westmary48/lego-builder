c058d666dfc8f6e3955896e213b0a1fd5ef95d9f
Mary West <westmarye1219@gmail.com>
2018-11-07 18:53:38 -0600

    added string builders

--- src/components/LegoCharacter/legoCharacter.js
+++ src/components/LegoCharacter/legoCharacter.js
@@ -1,4 +1,50 @@
 import 'jquery';
 import 'bootstrap';
 
-console.log('legCharacter');
+
+const headBuilder = (head) => {
+  const domString = `
+    <li class="player-team-${head.id}">
+      <div class="img-holder">
+        <img class="player-img" src="${head.imgURL}">
+      </div>
+      <h3 class="header-name">${head.name}</h3>
+    </li>
+  `;
+
+  return domString;
+};
+
+const torsosBuilder = (torso) => {
+  const domString = `
+      <li class="player-team-${torso.id}">
+        <div class="img-holder">
+          <img class="player-img" src="${torso.imgURL}">
+        </div>
+        <h3 class="header-name">${torso.name}</h3>
+      </li>
+    `;
+
+  return domString;
+};
+
+const legsBuilder = (leg) => {
+  const domString = `
+      <li class="player-team-${leg.id}">
+        <div class="img-holder">
+          <img class="player-img" src="${leg.imgURL}">
+        </div>
+        <h3 class="header-name">${leg.name}</h3>
+      </li>
+    `;
+
+  return domString;
+};
+
+const legoBuilder = () => {
+  headBuilder();
+  torsosBuilder();
+  legsBuilder();
+};
+
+legoBuilder();
